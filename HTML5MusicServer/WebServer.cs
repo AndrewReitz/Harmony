5cf54c9157d2f8e9fc7ee0031ad30a5357b6a8e0
Andrew <deadhead1337@yahoo.com>
2012-02-25 22:53:25 -0600

    first commit

---
+++ HTML5MusicServer/WebServer.cs
@@ -0,0 +1,252 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading;
+using System.IO;
+using System.Xml.Linq;
+using System.Security.Cryptography;
+
+namespace HTML5MusicServer
+{
+    public class WebServer
+    {
+        HttpListener _listener;
+        string _musicDirectory;
+        string _javaScriptDir;
+        string _skins;
+
+        //it was just easier to do it this way instead of using XElement
+        string _audioPlayer_HTML;
+
+        public WebServer(string MusicDirectory, int port)
+        {
+            ThreadPool.SetMinThreads(50, 50);
+            ThreadPool.SetMaxThreads(500, 1000);
+            _listener = new HttpListener();
+            _listener.Prefixes.Add("http://*:" + port + "/");
+            _musicDirectory = MusicDirectory;
+            _javaScriptDir = Path.Combine(Directory.GetCurrentDirectory(), "js");
+            _skins = Path.Combine(Directory.GetCurrentDirectory(), "skin");
+            _audioPlayer_HTML = File.ReadAllText(Path.Combine(Directory.GetCurrentDirectory(), "audio_player.html"));
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+
+            Thread t = new Thread(Run);
+            t.Start();
+        }
+
+        private void Run()
+        {
+            while (_listener.IsListening)
+            {
+                HttpListenerContext request = _listener.GetContext();
+                ThreadPool.QueueUserWorkItem(ProcessRequest, request);
+            }
+        }
+
+        public void Stop()
+        {
+            _listener.Stop();
+        }
+
+        public bool IsListening()
+        {
+            return _listener.IsListening;
+        }
+
+        void ProcessRequest(object listenerContext)
+        {
+            HttpListenerContext context = (HttpListenerContext)listenerContext;
+
+            //string msg = context.Request.HttpMethod + " " + context.Request.Url;
+            //Console.WriteLine(msg);
+
+            context.Response.StatusCode = (int)HttpStatusCode.OK;
+            context.Response.SendChunked = true;
+            context.Response.KeepAlive = true;
+            context.Response.ProtocolVersion = HttpVersion.Version11;
+            context.Response.AddHeader("Connection", "Keep-Alive");
+            context.Response.AddHeader("Keep-Alive", "timeout=15, max=100");
+            //context.Response.Headers.Add("Content-Encoding: gzip");
+
+            byte[] b = null;
+            switch (context.Request.RawUrl)
+            {
+                case "/": b = GetWebPage(_musicDirectory); break;
+                default:
+                    string filePath = Path.Combine(_musicDirectory, Uri.UnescapeDataString(context.Request.RawUrl.Replace("/", "\\").Remove(0, 1)));
+                    if (context.Request.RawUrl.Contains("/skin/"))
+                    {
+                        b = GetFileBytes(Path.Combine(_skins, Path.GetFileName(context.Request.RawUrl)));
+                    }
+                    else if (context.Request.RawUrl.Contains("/js/"))
+                    {
+                        b = GetFileBytes(Path.Combine(_javaScriptDir, Path.GetFileName(context.Request.RawUrl)));
+                    }
+                    else if (File.Exists(filePath))
+                    {
+                        switch (Path.GetExtension(filePath))
+                        {
+                            case ".mp3": context.Response.ContentType = "audio/mpeg"; b = GetFileBytes(filePath); break;
+                            case ".m4a": context.Response.ContentType = "audio/mp4"; b = GetFileBytes(filePath); break;
+                            case ".flac": context.Response.ContentType = "audio/x-flac"; b = GetFileBytes(filePath); break; //try but doubt it will work
+                            case ".mp4": context.Response.ContentType = "audio/mp4"; b = GetFileBytes(filePath); break;
+                            case ".ogg": context.Response.ContentType = "audio/ogg"; b = GetFileBytes(filePath); break;
+                            case ".wav": context.Response.ContentType = "audio/wav"; b = GetFileBytes(filePath); break;
+                            default: b = NotFound(context); break;
+                        }
+
+                        context.Response.AddHeader("ETag", GetMD5(b));
+                        context.Response.AddHeader("Last-Modified", GetLastModifiedDate(filePath));
+                    }
+                    else if (Directory.Exists(filePath))
+                    {
+                        b = GetWebPage(filePath);
+                    }
+                    else
+                    {
+                        b = NotFound(context);
+                    }
+                    break;
+            }
+
+
+            int rangeBegin = 0;
+            int rangeEnd = b.Length;
+            string range = context.Request.Headers["Range"];
+
+            if (range != null)
+            {
+                string[] temp = range.Replace("bytes=", "").Split('-');
+                Int32.TryParse(temp[0], out rangeBegin);
+                Int32.TryParse(temp[1], out rangeEnd);
+                if (rangeEnd == 0)
+                {
+                    rangeEnd = b.Length;
+                }
+
+                context.Response.AddHeader("Content-Range", rangeBegin + "-" + (rangeEnd - rangeBegin) + "/" + rangeEnd);
+            }
+
+            context.Response.ContentLength64 = b.Length;
+
+            try
+            {
+                //add in gzip when this is running smoothly
+
+                context.Response.OutputStream.Write(b, 0, b.Length);
+                context.Response.OutputStream.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// Adds all the music in the directory to the file and returns it's bytes
+        /// </summary>
+        /// <param name="directory">directory to get all the directory/music files</param>
+        /// <returns>byte array of the generated webpage</returns>
+        byte[] GetWebPage(string directory)
+        {
+            //getting all the music and creating it in jason format to
+            //to put into the file
+            StringBuilder json_music = new StringBuilder();
+            foreach (string file in Directory.GetFiles(directory))
+            {
+                switch (Path.GetExtension(file))
+                {
+                    //trying any and all, will put them in as type mp3...
+                    case ".mp3":
+                        json_music.Append(getMusic(file));
+                        break;
+                    case ".m4a":
+                        json_music.Append(getMusic(file));
+                        break;
+                    case ".flac": //don't this this type is supported by jplayer
+                        json_music.Append(getMusic(file));
+                        break;
+                    case ".mp4":
+                        json_music.Append(getMusic(file));
+                        break;
+                    case ".ogg":
+                        json_music.Append(getMusic(file));
+                        break;
+                    case ".wav":
+                        json_music.Append(getMusic(file));
+                        break;
+                }
+            }
+
+            //getting all the directories
+            StringBuilder directories = new StringBuilder();
+            foreach (string dir in Directory.GetDirectories(directory))
+            {
+                directories.Append(
+                    "<a href=\"" +
+                    dir.Replace(_musicDirectory, "").Replace("\\", "/") +
+                    "\">" + dir.Replace(_musicDirectory, "").Replace("\\", "/") + "</a><br>"
+                    );
+            }
+
+            string returnString = _audioPlayer_HTML.Replace("##Audio_Here##", json_music.ToString());
+            returnString = returnString.Replace("#Directories_Here#", directories.ToString());
+
+
+            return Encoding.UTF8.GetBytes(returnString);
+        }
+
+        string getMusic(string file)
+        {
+            return "\t{\n\tname:\"" + Path.GetFileName(file) + "\",\n\tmp3:\"" + file.Replace(_musicDirectory, "").Replace("\\", "/") + "\"\n\t},";
+        }
+
+        byte[] NotFound(HttpListenerContext context)
+        {
+            context.Response.StatusCode = (int)HttpStatusCode.NotFound;
+            XDocument doc = new XDocument(
+                new XElement("html",
+                    new XElement("head",
+                        new XElement("title", "Error 404 (Not Found)")),
+                    new XElement("body",
+                        new XElement("h1", "404 file not found"))));
+
+            return Encoding.UTF8.GetBytes("<!DOCTYPE html>" + doc.ToString());
+        }
+
+        byte[] GetFileBytes(string filePath)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Open, FileAccess.Read))
+            {
+                byte[] returnBytes = new byte[fs.Length];
+                fs.Read(returnBytes, 0, (int)fs.Length);
+                return returnBytes;
+            }
+        }
+
+        string GetMD5(byte[] bytes)
+        {
+            MD5 md5 = MD5.Create();
+
+            StringBuilder md5Sb = new StringBuilder();
+            foreach (byte b in md5.ComputeHash(bytes))
+            {
+                md5Sb.Append(b.ToString("x2"));
+            }
+
+            return md5Sb.ToString();
+        }
+
+        string GetLastModifiedDate(string filePath)
+        {
+            DateTime lastWriteTimeUtc = File.GetLastWriteTimeUtc(filePath);
+            return string.Format("{0:ddd, dd MMM yyyy HH:mm:ss} GMT", lastWriteTimeUtc);
+        }
+    }
+}
---
+++ Harmony/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Forms;
+using HTML5MusicServer;
+
+namespace Harmony
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        WebServer _server;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select a Folder that contains your music";
+                dialog.ShowNewFolderButton = false;
+                dialog.RootFolder = Environment.SpecialFolder.MyComputer;
+
+                if (!string.IsNullOrEmpty(this.textBlockBrowseContent.Text))
+                {
+                    dialog.SelectedPath = this.textBlockBrowseContent.Text;
+                }
+                else
+                {
+                    dialog.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyMusic);
+                }
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    this.textBlockBrowseContent.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void buttonStart_Click(object sender, RoutedEventArgs e)
+        {
+            string errorMsg = "";
+            string musicDir = this.textBlockBrowseContent.Text;
+            string port = this.textBoxPort.Text;
+
+            if (string.IsNullOrEmpty(musicDir))
+            {
+                errorMsg = "You are missing a music directory\n";
+            }
+            if (string.IsNullOrEmpty(port))
+            {
+                errorMsg += "You are missing a port number";
+            }
+
+            if (!string.IsNullOrEmpty(errorMsg))
+            {
+                System.Windows.MessageBox.Show(errorMsg, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            else
+            {
+                int portNum;
+                if (Int32.TryParse(port, out portNum))
+                {
+                    //check if null for first run
+                    if (_server == null || !_server.IsListening())
+                    {
+                        _server = new WebServer(musicDir, portNum);
+                        _server.Start();
+                    }
+                }
+                else
+                {
+                    System.Windows.MessageBox.Show("Invalid Port Number", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+        }
+
+        private void buttonStop_Click(object sender, RoutedEventArgs e)
+        {
+            if (_server != null && _server.IsListening())
+            {
+                _server.Stop();
+            }
+        }
+    }
+}
